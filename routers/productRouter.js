ea57464fad649cd05404327010662da41a728156
Uveis DZ <uveis2005@gmail.com>
2025-11-13 14:44:54 +0300

    Update productRouter.js

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -5,13 +5,12 @@ const productController = require('../controllers/productController.js');
 const router = new Router();
 
 
-router.post('/product', ProductController.addProduct);
-router.get("/products", ProductController.getProducts);
-router.get("/product/:id", ProductController.getOneProduct);
+router.post('/product', productController.addProduct);
+router.get("/products", productController.getProducts);
+router.get("/product/:id", productController.getOneProduct);
 router.put("/product/update", productController.updateProduct);
 router.patch("/product/:id", productController.editProduct);
-// router.delete("/product/:id");
-
+router.delete("/product/:id", productController.deleteProduct); 
 
 
 module.exports = router
