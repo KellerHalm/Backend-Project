7330b4ecdd27c0818033d5c6f9ad426f0e2abdc5
DUM <uveis2005@gmail.com>
2025-11-27 15:01:36 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,8 +4,10 @@ const { Schema, model, default: mongoose } = require('mongoose');
 const User = new mongoose.Schema({
     username : { type: String, unique: true, required: true },
     password : { type: String, required: true },
-    role: [{ type: String, ref: 'Role' }]
+    role: [{ type: String, ref: 'Role' }],
+    avatar: { type: String }
 })
+
 
 
 module.exports = mongoose.model('User', User)
