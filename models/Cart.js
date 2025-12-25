ea660d53eb215fe0ab76803ff74ab2fb6f5843b6
DUM <uveis2005@yandex.ru>
2025-12-25 15:35:41 +0300

    Add files via upload

---
+++ models/Cart.js
@@ -0,0 +1,19 @@
+const { Schema, model, default: mongoose } = require('mongoose');
+
+
+const Cart = new mongoose.Schema({
+
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', require: true, unique: true },
+    products: [{ 
+        product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', require: true },
+        quantity: { type: Number, require: true, default: 1, min: 1 },
+        addedAt: { type: Date, default: Date.now },
+        totalPrice: { type: Number, default: 0 },
+        totalItems: { type: Number, default: 0 },
+        updateAt: { type: Date, default: Date.now }
+    }]
+
+})
+
+
+module.exports = mongoose.model('Cart', Cart);
